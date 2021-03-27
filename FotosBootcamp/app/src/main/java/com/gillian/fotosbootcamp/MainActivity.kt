09b2d3401cc11f7ba75a6b95fe42bd7a509f6fb8
brgillian <brgillian@gmail.com>
2021-03-26 22:28:58 -0300

    Verifica se o sdk do dispositivo é superior ao Marshmallow

--- FotosBootcamp/app/src/main/java/com/gillian/fotosbootcamp/MainActivity.kt
+++ FotosBootcamp/app/src/main/java/com/gillian/fotosbootcamp/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.gillian.fotosbootcamp
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        pick_button.setOnClickListener{
+            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){}
+        }
     }
 }
